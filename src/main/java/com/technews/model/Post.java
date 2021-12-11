1f80714c5349c28ea108b65e632c474996126765
Enrico Perkins Jr <evperkinsjr@gmail.com>
2021-12-11 11:52:48 -0500

    added class-level and instance variable-level annotations to Post class

--- src/main/java/com/technews/model/Post.java
+++ src/main/java/com/technews/model/Post.java
@@ -1,4 +1,26 @@
 package com.technews.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+@Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@Table(name = "post")
 public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String title;
+    private String postUrl;
+    @Transient
+    private String userName;
+    @Transient
+    private int voteCount;
+    private Integer userId;
+    private Date postedAt = new Date();
+    private Date updatedAt = new Date();
+    private List<Comment> comments;
+
 }
